1a1eb2c8b0378cd6cf23adbb1fe070495f9bc5c5
rparke <50015370+rparke@users.noreply.github.com>
2022-11-09 08:13:48 +0000

    made datasource generic

--- api_to_database/datasource.py
+++ api_to_database/datasource.py
@@ -3,47 +3,33 @@ import logging
 import datetime
 
 
-class DataSourceAuctionResults:
+class DataFrameClearner:
     """
     DataSource Object: for Database Consumer to Use
     """
 
-    def __init__(self, uri):
-        self.uri = uri
-        self.filter_companies = ["HABITAT ENERGY LIMITED"]
+    def __init__(self):
+        pass
 
     # Basic Placeholder function for validation, in practice MORE validation is neeeded.
     def _basic_validation(self):
         logging.info("Validating...")
         logging.info("Validation passed because validation will always pass")
 
-    def _rename_columns(self):
-        self._df = self._df.rename(
-            columns={
-                "_id": "id",
-                "Company": "company",
-                "Unit Name": "unit_name",
-                "EFA Date": "efa_date",
-                "Delivery Start": "delivery_start",
-                "Delivery End": "delivery_end",
-                "EFA": "efa",
-                "Service": "service",
-                "Cleared Volume": "cleared_volume",
-                "Clearing Price": "clearing_price",
-                "Technology Type": "technology_type",
-                "Location": "location",
-                "Cancelled": "cancelled",
-            }
-        )
+    def _rename_columns(self, rename_mapping):
+        self._df = self._df.rename(columns=rename_mapping)
 
-    def _filter_out_companies(self):
-        self._df = self._df[self._df["company"].isin(self.filter_companies)]
+    def _filter_out_companies(self, filter_companies):
+        self._df = self._df[self._df["company"].isin(filter_companies)]
 
-    def get_data_as_df(self):
-        self._df = pd.read_csv(
-            self.uri,
-        )
+    def get_data_as_df(
+        self,
+        df,
+        filter_companies,
+        rename_mapping,
+    ):
+        self._df = df
         self._basic_validation()
-        self._rename_columns()
-        self._filter_out_companies()
+        self._rename_columns(rename_mapping)
+        self._filter_out_companies(filter_companies)
         return self._df
--- api_to_database/main.py
+++ api_to_database/main.py
@@ -1,12 +1,14 @@
 import logging
-from datasource import DataSourceAuctionResults
+from datasource import DataFrameClearner
 from database_service import AuctionResultsDatabaseService
 from sqlalchemy import create_engine
+import pandas as pd
 
 URI = "https://data.nationalgrideso.com/backend/datastore/dump/ddc4afde-d2bd-424d-891c-56ad49c13d1a"
 database_url = (
     "sqlite://"  # we are simply using an in memory sqlite db for demonstration here
 )
+filter_companies = ["HABITAT ENERGY LIMITED"]
 
 
 def main():
@@ -17,7 +19,26 @@ def main():
     db_service = AuctionResultsDatabaseService()
     db_service.create_auction_results_table(engine=engine)
     db_service.add_results_to_database(
-        engine=engine, df=DataSourceAuctionResults(URI).get_data_as_df()
+        engine=engine,
+        df=DataFrameClearner().get_data_as_df(
+            pd.read_csv(URI),
+            filter_companies=filter_companies,
+            rename_mapping={
+                "_id": "id",
+                "Company": "company",
+                "Unit Name": "unit_name",
+                "EFA Date": "efa_date",
+                "Delivery Start": "delivery_start",
+                "Delivery End": "delivery_end",
+                "EFA": "efa",
+                "Service": "service",
+                "Cleared Volume": "cleared_volume",
+                "Clearing Price": "clearing_price",
+                "Technology Type": "technology_type",
+                "Location": "location",
+                "Cancelled": "cancelled",
+            },
+        ),
     )
 
 
