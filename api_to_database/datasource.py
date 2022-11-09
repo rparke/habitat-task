68b4f8bfea6d72bf87dbc5699bdb78399de32091
rparke <50015370+rparke@users.noreply.github.com>
2022-11-09 08:20:37 +0000

    added placeholder docstrings

--- api_to_database/datasource.py
+++ api_to_database/datasource.py
@@ -5,21 +5,28 @@ import datetime
 
 class DataFrameClearner:
     """
-    DataSource Object: for Database Consumer to Use
+    Generic DataFrame Cleaner. Performs validation of the incomming dataframe,
+    renames columns so they adhere to the tables schema, and filters out companies not of interest
     """
 
     def __init__(self):
         pass
 
-    # Basic Placeholder function for validation, in practice MORE validation is neeeded.
     def _basic_validation(self):
+        """Placeholder Function for Validation"""
         logging.info("Validating...")
         logging.info("Validation passed because validation will always pass")
 
     def _rename_columns(self, rename_mapping):
+        """Generic Function for Renaming Columns to Fit to a Schema"""
         self._df = self._df.rename(columns=rename_mapping)
 
     def _filter_out_companies(self, filter_companies):
+        """
+        Problem specific function to filter out specific comapnies.
+        Going forward this should probably be rewritten as more generic
+        by passing a dict of key value pairs containing the key to filter, and the values to filter out
+        """
         self._df = self._df[self._df["company"].isin(filter_companies)]
 
     def get_data_as_df(
